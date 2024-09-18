f95754d45b7b0ec74618ef6b78f0aee58387b749
Maureen <maureendelafuente.b@gmail.com>
2024-09-18 16:24:33 +0200

    Add project files.

---
+++ Encapsulation/PersonHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.Design.Serialization;
+using System.Runtime.Versioning;
+
+namespace Encapsulation;
+
+public class PersonHandler
+{    
+    public void SetAge(Person pers, int age)
+    {//set property Age of pers to age
+        pers.Age = age;
+    }
+
+    public Person CreatePerson(int age, string fname, string lname, double height, double weight)
+    {
+        Person p = new Person(fname, lname);
+        p.Age = age;
+        p.Height = height;
+        p.Weight = weight;
+        return p;
+    }
+    /*    5. När denna klass är klar, kommentera bort er tidigare instans av Person från
+Program.cs, och instansiera istället en PersonHandler. Skapa därigenom några
+personer och testa era metoder.
+        */
+    public void ChangeName(Person pers, string fname, string lname)
+    {
+        pers.FName = fname;
+        pers.LName = lname;
+    }
+    public void ChangeHeight(Person pers, double height)
+    {
+        pers.Height = height;
+    }
+    public void ChangeWeight(Person pers, double weight)
+    {
+        pers.Weight = weight;
+    }
+}
---
+++ Encapsulation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            try
+            {
+                Person p = new Person("Elon", "Musk");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            //p.age;
+            //p.Age;
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
